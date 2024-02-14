b7a33e24764e6c3e4d3ce6a5359deb992f98f0c1
Takashi Aoki <arfaben_5816@icloud.com>
2024-02-15 01:10:06 +0900

    feat:hello-vue

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref('田中太郎');
+</script>
+
+<template>
+</template>
+<div id="app">
+  <h1>こんにちは！{{name}}さん</h1>
+</div>
+<style scoped>
+</style>
